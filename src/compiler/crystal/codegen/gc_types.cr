de86a886277565fd6c6076780d538a920e2950bc
ffwff <mogamiriver@tuta.io>
2020-01-03 21:53:53 +0700

    create gc bitmaps for structs

--- src/compiler/crystal/codegen/gc_types.cr
+++ src/compiler/crystal/codegen/gc_types.cr
@@ -45,23 +45,38 @@ module Crystal
             case ivar.type
             when VirtualType
               if ivar.type.as(VirtualType).struct?
-                print "offsets: ", malloc_offsets(ivar.type), '\n' if dump
-                raise "handling struct is unimplemented"
+                soffset = malloc_offsets(ivar.type)
               end
             when ClassType
               if ivar.type.as(ClassType).struct?
-                print "offsets: ", malloc_offsets(ivar.type), '\n' if dump
-                raise "handling struct is unimplemented"
+                soffset = malloc_offsets(ivar.type)
               end
             end
             if is_struct
-              offset = @program.offset_of(type.sizeof_type, idx)
+              base_offset = @program.offset_of(type.sizeof_type, idx)
             else
-              offset = @program.instance_offset_of(type.sizeof_type, idx)
+              base_offset = @program.instance_offset_of(type.sizeof_type, idx)
             end
-            bit = offset // llvm_typer.pointer_size
-            offsets |= (1u64 << bit.to_u64)
-            puts " + #{name}, #{ivar.type}, #{offset}" if dump
+            if soffset
+              base_bit = base_offset // llvm_typer.pointer_size
+              i = 0
+              while soffset != 0
+                if (soffset & 1) != 0
+                  bit = base_bit + i
+                  offsets |= (1u32 << bit.to_u32)
+                  puts " + [mixed] #{name}, #{ivar.type}, #{bit*llvm_typer.pointer_size}" if dump
+                else
+                  bit = base_bit + i
+                  puts " - [mixed] #{name}, #{ivar.type}, #{bit*llvm_typer.pointer_size}" if dump
+                end
+                soffset >>= 1
+                i += 1
+              end
+            else
+              bit = base_offset // llvm_typer.pointer_size
+              offsets |= (1u64 << bit.to_u64)
+              puts " + #{name}, #{ivar.type}, #{base_offset}" if dump
+            end
           end
         else
           if is_struct
